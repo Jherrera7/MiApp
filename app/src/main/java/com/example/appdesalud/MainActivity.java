3509bae01a80f17f616fbaa0e7dee7bcd1b1097a
Jesus Herrera <jesus3846@hotmail.com>
2020-02-25 17:03:34 -0500

    Avance de app

---
+++ app/src/main/java/com/example/appdesalud/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.appdesalud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Enterokay (View j){
+        EditText user = findViewById(R.id.user);
+
+    }
+}
